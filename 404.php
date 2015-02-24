6bc8c911dfb3efe50f197f67e9c0da9f96b46b2b
John Penyah <johnp@constantclick.com>
2015-02-24 13:41:02 +0800

    test

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
     <div id="error" class="container">
         <h1><?php _e( '404, Page not found', ZEETEXTDOMAIN );?> </h1>
         <p><?php _e( 'The Page you are looking for doesnt exist or an other error occurred', ZEETEXTDOMAIN );?> </p>
-        <a class="btn btn-success" href="<?php echo home_url(); ?>"><?php _e( 'GO BACK TO THE HOMEPAGE', ZEETEXTDOMAIN );?></a>
+        <a class="btn btn-success" href="<?php echo home_url(); ?>"><?php _e( 'Go Back To The Homepage', ZEETEXTDOMAIN );?></a>
     </div><!--/#error-->
 </div><!-- #content -->
 <?php get_footer();
